a6bec5ac8a81f50f8adb078fef44b5ef853db39d
simran <simrantyagi14342gmail.com>
2025-11-05 18:42:40 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import NavBar from "./components/Navbar/Navbar";
+import Header from "./components/Header/Header";
+import Dashboard from "./components/Dashboard/Dashboard";
+
+const App = () => {
+  return (
+    <>
+      <NavBar />
+      <Header />
+      <Dashboard />
+    </>
+  );
+};
+
+export default App;
